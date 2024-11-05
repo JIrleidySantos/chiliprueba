409ef2dcece3e8c8215f61dec977f357cd0f3cf0
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-05 00:33:28 -0500

    chili chili chili chili x2

---
+++ src/main/java/com/mycompany/poyectchili2/Poyectchili2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poyectchili2;
+
+/**
+ *
+ * @author mundo
+ */
+public class Poyectchili2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
